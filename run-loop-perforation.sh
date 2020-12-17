587faa6420cd7845d4a2e28dfaabe6264210feae
Lavinia Miranda <laviniameds@icloud.com>
2020-12-17 10:10:17 -0300

    update run script

--- run-loop-perforation.sh
+++ run-loop-perforation.sh
@@ -1,22 +1,38 @@
 #!/bin/bash
 
-# build project
 #check if directory 'build' exists, if true: proceeds, else: create directory
 if [ -d build ]; then :  
 else mkdir build 
 fi 
 
+# build project
 cd build/ && cmake .. && make && cd .. && echo ""\
 
-# testing pass
 src=$1
+src_name=${1%.c}
+standard=${1%/*.c}/results/standard
+perforrated=${1%/*.c}/results/perforrated
+
 obj=${1%.c}.ll
 opt=${1%.c}.opt.ll
 
-clang-10 -S -emit-llvm ${src} -g3 -O0 -Xclang -disable-O0-optnone -o ${obj}
+#run the example and generate the 'standard' result
+clang-10 -Wall $src -o $src_name
+./$src_name > $standard
 
-opt-10 -S -mem2reg ${obj} > ${opt}
+#run the perforation pass and get the modified example
 
-#opt -analyze -dot-cfg ${opt}
+# # pass testing 
+# clang-10 -S -emit-llvm ${src} -g3 -O0 -Xclang -disable-O0-optnone -o ${obj}
 
-opt-10 -load build/loop-perforation/libLoopPerforationPass.so -loop-perforation < ${opt} > /dev/null
+# opt-10 -S -mem2reg ${obj} > ${opt}
+
+# #opt -analyze -dot-cfg ${opt}
+
+# opt-10 -load build/loop-perforation/libLoopPerforationPass.so -loop-perforation < ${opt} > /dev/null
+
+#TODO
+#run modified example and generates the 'perforrated' result
+
+#TODO 
+#run the error metrics to get final results
