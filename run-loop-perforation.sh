415a75bd34862100638d71588b917f56f8b6657d
Lavinia Miranda <laviniameds@icloud.com>
2020-12-22 12:23:36 -0300

    clean run script

--- run-loop-perforation.sh
+++ run-loop-perforation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#check if directory 'build' exists, if true: proceeds, else: create directory
+# check if directory 'build' exists, if true: proceeds, else: create directory
 if [ -d build ]; then :  
 else mkdir build 
 fi 
@@ -8,7 +8,7 @@ fi
 # build project
 cd build/ && cmake .. && make && cd .. && echo ""\
 
-#define variables
+# define variables
 src=$1
 src_name=${1%.c}
 standard=${1%/*.c}/results/standard.txt
@@ -22,15 +22,15 @@ error_metrics=${1%/*.c}/error_metrics/error_metrics.c
 error_metrics_name=${error_metrics%.c}
 final_results=${1%/*.c}/results/final_results.txt
 
-#run the example and generate the 'standard' result
+# run the example and generate the 'standard' result
 clang-10 -Wall $src -o $src_name
 ./$src_name > $standard
 standard_result=$(<${standard})
 
-#get rates from file
+# get rates from file
 rates=$(<${rates_path})
 
-#init final results file
+# init final results file
 if [ -e $final_results ]; then 
     > $final_results
     echo -e "--- RESULTS: ---\n" >> $final_results
@@ -40,25 +40,25 @@ fi
 
 # run pass 
 clang-10 -S -emit-llvm ${src} -g3 -O0 -Xclang -disable-O0-optnone -o ${obj}
-
+# drop memory accesses
 opt-10 -S -mem2reg ${obj} > ${opt}
 
-#test perforation
+# test perforation
 for i in $rates
 do
     perf_option=${perf%.c}_${i}.opt.ll
     perf_option_name=${perf%.c}_${i}
 
-    #generate perforated code variants
+    # generate perforated code variants
     opt-10 -S -load build/loop-perforation/libLoopPerforationPass.so -loop-perforation -loop_rate=$i < ${opt} > ${perf_option}
     clang-10 -Wall $perf_option -o $perf_option_name
 
-    #get perforated result
+    # get perforated result
     perf_result=$(./$perf_option_name)
     #merge standard and perforated results
     echo "$standard_result $perf_result" > $merged_results
 
-    #test error metrics function
+    # test error metrics function
     clang-10 -Wall ${error_metrics} -o ${error_metrics_name}
     error_ratio=$(./$error_metrics_name < $merged_results)
 
