ac5596ffe853a75ae1fc1b2e9d7997d5b62f6286
Lavinia Miranda <laviniameds@icloud.com>
2021-04-16 19:17:48 -0300

    rename run-profiling script and update CMakeLists

--- run-profiling.sh
+++ run-profiling.sh
@@ -11,10 +11,7 @@ cd build/ && cmake .. && make && cd .. && echo ""\
 # define variables
 src=$1
 obj=${1%.c}.ll
-opt=${1%.c}.opt.ll
 
 clang-12 -S -emit-llvm ${src} -g3 -O0 -Xclang -disable-O0-optnone -o ${obj}
 
-opt-12 -S -mem2reg ${obj} > ${opt}
-
-opt-12 -disable-output -load-pass-plugin=build/analysis/profiling/libProfiling.so -passes="profiling" ${opt} 
+opt-12 -disable-output -load-pass-plugin=build/analysis/profiling/libProfiling.so -passes="profiling" ${obj} 
