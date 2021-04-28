c2de505fb31951715741e5a1368df94e83167371
Lavinia Miranda <laviniameds@icloud.com>
2021-04-28 20:31:47 -0300

    update profiling with diff between binary instructions

--- analysis/profiling/Profiling.cpp
+++ analysis/profiling/Profiling.cpp
@@ -1,12 +1,13 @@
 #include "llvm/Pass.h"
 #include "llvm/IR/Module.h"
+#include "llvm/IR/Function.h"
 #include "llvm/IR/PassManager.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
 #include "fstream"
-#include "../../util/InstType.hpp"
+#include "../../util/opUtil.hpp"
 #include "llvm/Analysis/LoopInfo.h"
 #include "llvm/Analysis/LoopPass.h"
 
@@ -15,66 +16,83 @@ using namespace act;
 
 namespace {
 
-	//define llvm pass
-	struct Profiling : public PassInfoMixin<Profiling>{
+//define llvm pass
+struct Profiling : public PassInfoMixin<Profiling>{
 
-		std::string pass_name = "";
-		std::map<std::string, float> map_qtd_types;
-		std::map<std::string, float>::iterator it;
-		int total_qtd_instr = 0;
-		std::ofstream file;
-		std::string I_type;
+	std::string pass_name = "";
+	std::map<std::string, float> map_qtd_types;
+	std::map<std::string, float>::iterator it;
+	int total_qtd_instr = 0;
+	int total_loops = 0;
+	std::ofstream file;
+	std::string I_Name;
+	//StringRef *LoopBody = new StringRef("for.body");
 
-		void printResults(){
-			for(it = map_qtd_types.begin(); it != map_qtd_types.end(); it++){
-				file << it->first << " : " << (it->second*100)/total_qtd_instr << "%\n";
+	void printResults(){
+		for(it = map_qtd_types.begin(); it != map_qtd_types.end(); it++){
+			file << it->first << " : " << (it->second*100)/total_qtd_instr << "%\n";
+		}
+	}
+	
+	//TODO: quatify loops
+	// void processLoop(Loop *loop){			
+	// 	total_loops++;
+
+	// 	//check also the subloops into each loop
+	// 	for(Loop *sub_loop : loop->getSubLoops())
+	// 		processLoop(sub_loop);					
+	// }
+
+	// void runOnLoops(Function &F, FunctionAnalysisManager &FAM){
+	// 	LoopInfo LI;
+	// 	LI.analyze(FAM.getResult<DominatorTreeAnalysis>(F));
+
+	// 	for(auto &loop : LI)
+	// 		processLoop(loop);
+	// }
+
+	void runOnFunction(Function &F, FunctionAnalysisManager &FAM){
+		//run on each function basic block
+		for (auto &BB : F.getBasicBlockList()){
+			for (auto &I : BB.getInstList()){
+				I_Name = opUtil::getInstructionName(I.getOpcode());
+				
+
+				it = map_qtd_types.find(I_Name);						
+				if(it != map_qtd_types.end())
+					it->second++;
+				else
+					map_qtd_types.insert({I_Name, 1});
+
+				total_qtd_instr++;	
 			}
 		}
 
-		void runOnLoops(Function &F, FunctionAnalysisManager &FAM){
-			LoopInfo LI;
-			LI.analyze(FAM.getResult<DominatorTreeAnalysis>(F));
+		//run on function loops
+		//runOnLoops(F, FAM);
+	}
 
-			//TODO: quantify loops
-		}
+	PreservedAnalyses run(Module &M, ModuleAnalysisManager &MAM){
+		std::string modulename = M.getSourceFileName().substr(M.getSourceFileName().find_last_of("/")+1);
+		std::string filename("./analysis/profiling/results/");
+		filename.append(modulename);
+		filename.append(".txt");
+		file.open(filename.c_str(), std::ios::out);
+		file << "Profiling Results of '" << modulename << "'\n\n";
 
-		void runOnFunction(Function &F, FunctionAnalysisManager &FAM){
+		file << "-------- Function begin---------" << "\n";
+		FunctionAnalysisManager &FAM = MAM.getResult<FunctionAnalysisManagerModuleProxy>(M).getManager();
 
-			//run on function loops
-			runOnLoops(F, FAM);
+		for(auto &F : M.getFunctionList())
+			runOnFunction(F, FAM);				
 
-			//run on each function basic block
-			for (auto &BB : F.getBasicBlockList()){
-				for (auto &I : BB.getInstList()){
-						I_type = InstType::getITypeName(I.getOpcode());
-						it = map_qtd_types.find(I_type);						
-						if(it != map_qtd_types.end())
-							it->second++;
-						else
-							map_qtd_types.insert({I_type, 1});
+		printResults();
+		file << "-------- Function end---------" << "\n\n";
 
-						total_qtd_instr++;	
-					}
-				}
-		}
-
-		PreservedAnalyses run(Module &M, ModuleAnalysisManager &MAM, FunctionAnalysisManager &FAM){
-			std::string modulename = M.getSourceFileName().substr(M.getSourceFileName().find_last_of("/")+1);
-			std::string filename("./analysis/profiling/results/");
-			filename.append(modulename);
-			filename.append(".txt");
-			file.open(filename.c_str(), std::ios::out);
-			file << "Profiling Results of '" << modulename << "'\n\n";
-
-			for(auto &F : M.getFunctionList())
-				runOnFunction(F, FAM);
-			
-			printResults();
-
-			file.close();
-			return PreservedAnalyses::all();
-		}
-	};
+		file.close();
+		return PreservedAnalyses::all();
+	}
+};
 
 } // namespace llvm
 
---
+++ analysis/profiling/opUtil.cpp
@@ -0,0 +1,55 @@
+#include "../../util/opUtil.hpp"
+
+namespace act {
+
+std::string opUtil::getInstructionName(const unsigned llvm_opCpde){
+    if(llvm_opCpde >= llvm::Instruction::BinaryOps::BinaryOpsBegin && llvm_opCpde < llvm::Instruction::BinaryOpsEnd){
+        return opUtil::getBinaryName(llvm_opCpde);
+    }
+    else if (llvm_opCpde >= llvm::Instruction::TermOpsBegin && llvm_opCpde < llvm::Instruction::TermOpsEnd){
+        return "Control Flow";
+    }
+    else if (llvm_opCpde >= llvm::Instruction::MemoryOpsBegin && llvm_opCpde < llvm::Instruction::MemoryOpsEnd){
+        return "Memory";
+    }
+    else if (llvm_opCpde >= llvm::Instruction::UnaryOpsBegin && llvm_opCpde < llvm::Instruction::UnaryOpsEnd){
+        return "Float Point";
+    }
+    else if (llvm_opCpde >= llvm::Instruction::CastOpsBegin && llvm_opCpde < llvm::Instruction::CastOpsEnd){
+        return "Cast";
+    }
+    else if(llvm_opCpde >= llvm::Instruction::OtherOpsBegin && llvm_opCpde < llvm::Instruction::OtherOpsEnd){
+        return "Other";
+    }
+    else
+        return "Undefined";
+}
+
+std::string opUtil::getBinaryName(const unsigned llvm_opCpde){
+	switch (llvm_opCpde) {
+		case llvm::Instruction::BinaryOps::FAdd:
+		  return "Float Point";
+		  break;
+		case llvm::Instruction::BinaryOps::FDiv:
+		  return "Float Point";
+		  break;
+		case llvm::Instruction::BinaryOps::FMul:
+		  return "Float Point";
+		  break;
+		case llvm::Instruction::BinaryOps::FRem:
+		  return "Float Point";
+		  break;
+		case llvm::Instruction::BinaryOps::FSub:
+		  return "Float Point";
+		  break;
+		case llvm::Instruction::BinaryOps::UDiv:
+		  return "Float Point";
+		  break;
+		default:
+		  return "Integer";
+	}
+}
+
+
+
+}
