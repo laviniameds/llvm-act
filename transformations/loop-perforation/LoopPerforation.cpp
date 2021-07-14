2d78b30a0650e0735d96f07cb8c9dbf29cbc0f20
Lavinia Miranda <laviniameds@icloud.com>
2021-07-14 13:32:02 -0300

    add 3 new perforation methods

--- transformations/loop-perforation/LoopPerforation.cpp
+++ transformations/loop-perforation/LoopPerforation.cpp
@@ -3,6 +3,7 @@
 #include "llvm/Support/raw_ostream.h"
 #include "fstream"
 #include "llvm/Analysis/LoopInfo.h"
+#include "llvm/Analysis/ScalarEvolution.h"
 #include "llvm/Analysis/LoopPass.h"
 #include "llvm/Support/CommandLine.h"
 
@@ -96,48 +97,74 @@ namespace {
 			}
 		}
 
-		//perforate loop
-		void perforateLoops(){
-
-			for(it_f_loop_map = f_loop_map.begin(); it_f_loop_map != f_loop_map.end(); ++it_f_loop_map){
-
-				auto loop = it_f_loop_map->first;
-				auto llvm_function = it_f_loop_map->second;  
-
-				//get the PHI node correspondent to the canonical induction variable
-				PHINode *PHI = loop->getCanonicalInductionVariable();
-
-				//define a variable 'value to change'
-				Value *value_to_change = nullptr;
-
-				//foreach user from PHI 
-				for (auto User : PHI->users()) {
-					//foreach incoming value from PHI
-					for (auto &incoming : PHI->incoming_values()) {
-						//if matches, store the instruction and break
-						if (incoming == User) {
-							value_to_change = incoming;
-							break; 
-						}
-					}
-				}	
+		//modulo/interleaving
+		void perforateModulo(ScalarEvolution &se){
+			for(it_f_loop_map = f_loop_map.begin(); 
+			it_f_loop_map != f_loop_map.end(); ++it_f_loop_map){
 				
-				//replace induction variable with new value 'loop rate'
-				BinaryOperator *i = dyn_cast<BinaryOperator>(value_to_change);
-				for (auto &op : i->operands()) {
-					if (op == PHI) continue;
+				unsigned tripcount = 0;
+				unsigned maxtripcount = 0;
+				unsigned tripmultiple = 1;
+				BasicBlock *exitingblock = it_f_loop_map->first->getLoopLatch();
 
-					int loop_rate = 1;
-					loop_rate = InputLoopRate.getValue();
-					Type *ConstType = op->getType();
-					Constant *NewInc = ConstantInt::get(ConstType, loop_rate /*value*/, true /*issigned*/);
+				exitingblock = it_f_loop_map->first->getExitingBlock();
+				if (exitingblock){
+					tripcount = se.getSmallConstantMaxTripCount(it_f_loop_map->first);
+					tripmultiple = se.getSmallConstantTripMultiple(it_f_loop_map->first);
+					errs() << "trip count is " << tripcount <<"\n";
+					errs() << "trip multiple is " << tripmultiple <<"\n";
+				}
+				Optional<Loop::LoopBounds> bounds = it_f_loop_map->first->getBounds(se);
+				if (!bounds.hasValue()){
+					errs()<< "Did not get the bounds\n";
+				}
+				else{	
+					auto loop = it_f_loop_map->first;
+					auto llvm_function = it_f_loop_map->second;  
 
-					//errs() << "Changing [" << *op << "] to [" << *NewInc << "]!\n";
+					//get the PHI node correspondent to the canonical induction variable
+					PHINode *PHI = loop->getCanonicalInductionVariable();
 
-					op = NewInc;
-				}	
+					//define a variable 'value to change' and get the loop bound
+					Value *value_to_change = &bounds->getFinalIVValue();
+					
+					//replace the loop bound variable with new value
+					BinaryOperator *i = dyn_cast<BinaryOperator>(value_to_change);
+					for (auto &op : i->operands()) {
+						if (op == PHI) continue;
 
-			}				
+						string string_value = op->getNameOrAsOperand();
+						int int_value = stoi(string_value);
+
+						int loop_rate = 1;
+						loop_rate = (1 - InputLoopRate.getValue()) * int_value;
+						Type *ConstType = op->getType();
+						Constant *NewInc = ConstantInt::get(ConstType, loop_rate /*value*/, true /*issigned*/);
+
+						//errs() << "Changing [" << *op << "] to [" << *NewInc << "]!\n";
+
+						op = NewInc;
+					}	
+				}
+
+			}			
+		}
+		
+		//truncation
+		void perforateTruncation(){
+			
+		}
+		
+		//random
+		void perforateRandom(){
+			
+		}		
+
+		//perforate loop
+		void perforateLoops(ScalarEvolution &se){
+
+			perforateModulo(se);
+				
 		}
 		
 		virtual bool runOnFunction(Function &llvm_function){
@@ -147,12 +174,14 @@ namespace {
 				f_loop_map.clear();
 
 				LoopInfo &loop_info = getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
+				ScalarEvolution &se = getAnalysis<ScalarEvolutionWrapperPass>().getSE();
+
 				//handle the loops into every function
 				for(auto &loop : loop_info){
 					processLoop(llvm_function, loop);
 				}
 
-				perforateLoops();
+				perforateLoops(se);
 			}
 
 			return false;	    
