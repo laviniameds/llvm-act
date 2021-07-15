2ff037538407ce4d893ee5faabc75bf50978625b
Lavinia Miranda <laviniameds@icloud.com>
2021-07-15 19:12:27 -0300

    truncation perforation done

--- transformations/loop-perforation/LoopPerforation.cpp
+++ transformations/loop-perforation/LoopPerforation.cpp
@@ -11,16 +11,18 @@ using namespace llvm;
 using namespace std;
 
 //get input loop rate from command line
-static cl::opt<int> InputLoopRate("loop_rate", 
-cl::desc("Specify input loop_rate for Loop Perforation Pass"), 
-cl::value_desc("loop_rate"));
+static cl::opt<float> InputLoopRate("loop_rate",
+								  cl::desc("Specify input loop_rate for Loop Perforation Pass"),
+								  cl::value_desc("loop_rate"));
 
-namespace {
+namespace
+{
 	//define llvm pass
-	struct LoopPerforationPass : public FunctionPass {
-		
-		std::map<Loop*, Function*> f_loop_map;
-		std::map<Loop*, Function*>::iterator it_f_loop_map;
+	struct LoopPerforationPass : public FunctionPass
+	{
+
+		std::map<Loop *, Function *> f_loop_map;
+		std::map<Loop *, Function *>::iterator it_f_loop_map;
 
 		//define pass ID
 		static char ID;
@@ -28,29 +30,45 @@ namespace {
 		LoopPerforationPass() : FunctionPass(ID) {}
 
 		//LLVM Analysis
-		void getAnalysisUsage(AnalysisUsage &AU) const {
+		void getAnalysisUsage(AnalysisUsage &AU) const
+		{
 			AU.addRequired<LoopInfoWrapperPass>();
+			AU.addRequired<ScalarEvolutionWrapperPass>();
 			//AU.addRequiredID(LoopSimplifyID);
 		}
 
-		//return true or false if a loop is perforable
-		bool isPerforable(Function &llvm_function, Loop *loop){
-
-			//check if loop is simple
-			if (!loop->isLoopSimplifyForm()) {
-				return false;
+		//process a loop
+		void processLoop(Function &llvm_function, Loop *loop, ScalarEvolution &se)
+		{
+			//check if the actual loop is a good candidate to perforate
+			if (isPerforable(llvm_function, loop, se)){
+				//errs() << "That's a perforable loop!" << "!\n";
+				f_loop_map.insert({loop, &llvm_function});
 			}
+
+			//check also the subloops into each loop
+			for (Loop *sub_loop : loop->getSubLoops()){
+				processLoop(llvm_function, sub_loop, se);
+			}
+		}
+
+		//return true or false if a loop is perforable
+		//TODO: check perforation for loop bounds
+		bool isPerforable(Function &llvm_function, Loop *loop, ScalarEvolution &se)
+		{
+			//check if loop is simple
+			if (!loop->isLoopSimplifyForm())
+				return false;
 
 			//get the PHI node correspondent to the canonical induction variable
 			PHINode *PHI = loop->getCanonicalInductionVariable();
 
 			//if it's null, return false
-			if (PHI == nullptr) {
+			if (PHI == nullptr)
 				return false;
-			}
-
+			
 			//"find where the induction variable is modified by finding a user that
-    		// is also an incoming value to the phi"
+			// is also an incoming value to the phi"
 
 			//users: return the related instructions that uses the actual instruction
 			//incoming values: return the instruction operands
@@ -58,134 +76,126 @@ namespace {
 			//define a variable 'value to change'
 			Value *value_to_change = nullptr;
 
-			//foreach user from PHI 
-			for (auto User : PHI->users()) {
+			//foreach user from PHI
+			for (auto User : PHI->users())
+			{
 				//foreach incoming value from PHI
-				for (auto &incoming : PHI->incoming_values()) {
+				for (auto &incoming : PHI->incoming_values())
+				{
 					//if matches, store the instruction and break
-					if (incoming == User) {
+					if (incoming == User)
+					{
 						value_to_change = incoming;
-						break; 
+						break;
 					}
 				}
 			}
-			
+
 			//if it's null, return false
-			if (value_to_change == nullptr) {
+			if (value_to_change == nullptr)
 				return false;
-			}
 
 			//if it's not a binary operator, return false
-			if (!llvm::dyn_cast<llvm::BinaryOperator>(value_to_change)) {
+			if (!llvm::dyn_cast<llvm::BinaryOperator>(value_to_change))
 				return false;
-			}
 
 			return true;
 		}
 
-		//process a loop
-		void processLoop(Function &llvm_function, Loop *loop){
-			//check if the actual loop is a good candidate to perforate
-			if(isPerforable(llvm_function, loop)){
-				//errs() << "That's a perforable loop!" << "!\n";
-				f_loop_map.insert({loop, &llvm_function});
+		//truncation perforation
+		void perforateTruncation(ScalarEvolution &se){
+			for (it_f_loop_map = f_loop_map.begin(); it_f_loop_map != f_loop_map.end(); ++it_f_loop_map)
+			{
+				auto loop = it_f_loop_map->first;
+				auto llvm_function = it_f_loop_map->second;
+
+				PHINode *indVar = loop->getCanonicalInductionVariable();
+				BinaryOperator *indVarUpdate = NULL;
+				ICmpInst *cmp = NULL;
+
+				if (BasicBlock *latchBlock = loop->getExitingBlock())
+				{
+					for (auto &lbInst : *latchBlock)
+					{
+						if (auto *exitingBranch = dyn_cast<BranchInst>(&lbInst))
+						{
+							// branch must have a condition (which sets the loop bound)
+							if (exitingBranch->isConditional())
+							{
+								cmp = dyn_cast<ICmpInst>(exitingBranch->getCondition());
+								if (cmp)
+								{
+									Value *op1 = cmp->getOperand(0);
+									Value *op2 = cmp->getOperand(1);
+									Value *loopBound = op1 == indVar ? op2 : (op2 == indVar ? op1 : NULL);
+
+									// loop bound must be a constant. otherwise we can't vectorize
+									if (loopBound != NULL){
+										auto *loopBoundConst = dyn_cast<ConstantInt>(loopBound);
+										if (loopBoundConst){
+											float loop_rate = 0;
+											loop_rate = InputLoopRate.getValue();
+											Type *ConstType = loopBound->getType();
+											Constant *NewInc = ConstantInt::get(ConstType, (1 - loop_rate) * loopBoundConst->getZExtValue() /*value*/, true /*issigned*/);
+											
+											for(auto &op : cmp->operands()){
+												if(op == indVar) continue;
+												op = NewInc;
+											}
+										}
+									}
+									else
+										errs() << "no loop bound found!\n";
+								}
+							}
+							// else 
+							// 	errs() << "branch with no conditional!\n";
+						}
+						// else 
+						// 	errs() << "no exiting branch!\n";
+					}
+				}
+				// else 
+				// 	errs() << "no latchblock!\n";
 			}
-			
-			//check also the subloops into each loop
-			for(Loop *sub_loop : loop->getSubLoops()){
-				processLoop(llvm_function, sub_loop);
-			}
 		}
 
-		//modulo/interleaving
-		void perforateModulo(ScalarEvolution &se){
-			for(it_f_loop_map = f_loop_map.begin(); 
-			it_f_loop_map != f_loop_map.end(); ++it_f_loop_map){
-				
-				unsigned tripcount = 0;
-				unsigned maxtripcount = 0;
-				unsigned tripmultiple = 1;
-				BasicBlock *exitingblock = it_f_loop_map->first->getLoopLatch();
+		// //modulo/interleaving perforation
+		// void perforateModulo(){
 
-				exitingblock = it_f_loop_map->first->getExitingBlock();
-				if (exitingblock){
-					tripcount = se.getSmallConstantMaxTripCount(it_f_loop_map->first);
-					tripmultiple = se.getSmallConstantTripMultiple(it_f_loop_map->first);
-					errs() << "trip count is " << tripcount <<"\n";
-					errs() << "trip multiple is " << tripmultiple <<"\n";
-				}
-				Optional<Loop::LoopBounds> bounds = it_f_loop_map->first->getBounds(se);
-				if (!bounds.hasValue()){
-					errs()<< "Did not get the bounds\n";
-				}
-				else{	
-					auto loop = it_f_loop_map->first;
-					auto llvm_function = it_f_loop_map->second;  
+		// }
 
-					//get the PHI node correspondent to the canonical induction variable
-					PHINode *PHI = loop->getCanonicalInductionVariable();
+		// //random perforation
+		// void perforateRandom(){
 
-					//define a variable 'value to change' and get the loop bound
-					Value *value_to_change = &bounds->getFinalIVValue();
-					
-					//replace the loop bound variable with new value
-					BinaryOperator *i = dyn_cast<BinaryOperator>(value_to_change);
-					for (auto &op : i->operands()) {
-						if (op == PHI) continue;
-
-						string string_value = op->getNameOrAsOperand();
-						int int_value = stoi(string_value);
-
-						int loop_rate = 1;
-						loop_rate = (1 - InputLoopRate.getValue()) * int_value;
-						Type *ConstType = op->getType();
-						Constant *NewInc = ConstantInt::get(ConstType, loop_rate /*value*/, true /*issigned*/);
-
-						//errs() << "Changing [" << *op << "] to [" << *NewInc << "]!\n";
-
-						op = NewInc;
-					}	
-				}
-
-			}			
-		}
-		
-		//truncation
-		void perforateTruncation(){
-			
-		}
-		
-		//random
-		void perforateRandom(){
-			
-		}		
+		// }
 
 		//perforate loop
 		void perforateLoops(ScalarEvolution &se){
-
-			perforateModulo(se);
-				
+			perforateTruncation(se);
 		}
-		
-		virtual bool runOnFunction(Function &llvm_function){
-			if(!llvm_function.getName().equals_lower("main")){
+
+		virtual bool runOnFunction(Function &llvm_function)
+		{
+			if (!llvm_function.getName().equals_lower("main"))
+			{
 				//errs() << "I saw a function called " << llvm_function.getName() << "!\n";
-				
+
 				f_loop_map.clear();
 
 				LoopInfo &loop_info = getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
-				ScalarEvolution &se = getAnalysis<ScalarEvolutionWrapperPass>().getSE();
+				ScalarEvolution *se = &getAnalysis<ScalarEvolutionWrapperPass>().getSE();
 
 				//handle the loops into every function
-				for(auto &loop : loop_info){
-					processLoop(llvm_function, loop);
+				for (auto &loop : loop_info){
+					processLoop(llvm_function, loop, *se);
 				}
 
-				perforateLoops(se);
+				perforateLoops(*se);
 			}
 
-			return false;	    
-		}	    			
+			return false;
+		}
 	};
 
 } // namespace llvm
