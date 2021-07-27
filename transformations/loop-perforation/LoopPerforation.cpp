091893251fd45bbcd5dbc5f733b9bdd63348bb01
Lavinia Miranda <laviniameds@icloud.com>
2021-07-27 16:30:09 -0300

    reorganize functions

--- transformations/loop-perforation/LoopPerforation.cpp
+++ transformations/loop-perforation/LoopPerforation.cpp
@@ -26,8 +26,7 @@ static cl::opt<int> InputLoopMethod("loop_method",
 namespace
 {
 	//define llvm pass
-	struct LoopPerforationPass : public FunctionPass
-	{
+	struct LoopPerforationPass : public FunctionPass{
 
 		std::map<Loop *, Function *> f_loop_map;
 		std::map<Loop *, Function *>::iterator it_f_loop_map;
@@ -38,16 +37,14 @@ namespace
 		LoopPerforationPass() : FunctionPass(ID) {}
 
 		//LLVM Analysis
-		void getAnalysisUsage(AnalysisUsage &AU) const
-		{
+		void getAnalysisUsage(AnalysisUsage &AU) const{
 			AU.addRequired<LoopInfoWrapperPass>();
 			AU.addRequired<ScalarEvolutionWrapperPass>();
 			//AU.addRequiredID(LoopSimplifyID);
 		}
 
 		//process a loop
-		void processLoop(Function &llvm_function, Loop *loop)
-		{
+		void processLoop(Function &llvm_function, Loop *loop){
 			//check if the actual loop is a good candidate to perforate
 			if (isPerforable(llvm_function, loop)){
 				errs() << "That's a perforable loop: " << *loop << "!\n\n";
@@ -62,8 +59,7 @@ namespace
 
 		//return true or false if a loop is perforable
 		//TODO: check perforation for loop bounds
-		bool isPerforable(Function &llvm_function, Loop *loop)
-		{
+		bool isPerforable(Function &llvm_function, Loop *loop){
 			//check if loop is simple
 			if (!loop->isLoopSimplifyForm())
 				return false;
@@ -110,123 +106,118 @@ namespace
 			return true;
 		}
 
+		void perforateLoopBound(ICmpInst *cmp, Value* loopBound, PHINode *indVar, BasicBlock *bb){
+			errs() << "cmp: " << *cmp << "\n\n";
+			errs() << "loopBound: " << *loopBound << "\n\n";									
+			if (auto *loopBoundConst = dyn_cast<Instruction>(loopBound)){	
+				errs() << "loopBoundConst: " << *loopBoundConst << "\n";
+				errs() << "indVar: " << *indVar << "\n";									
+
+				//float y = (1 - perforation_rate)
+				float value = (1 - InputLoopRate.getValue());
+				//errs() << "value: " << value << "\n\n";
+				//if(v <= 0) v = 1;
+				auto *y = ConstantFP::get(Type::getFloatTy(bb->getContext()), value);					
+				errs() << "y: " << *y << "\n";
+
+				// (float)(loopBoundConst)
+				Instruction *loopBoundConst_float = SIToFPInst::Create(Instruction::CastOps::SIToFP, 
+				loopBoundConst,	Type::getFloatTy(bb->getContext()), "");
+				
+				loopBoundConst_float->insertAfter(loopBoundConst);
+
+				errs() << "loopBoundConst_float: " << *loopBoundConst_float << "\n";
+				
+				// float k = (y * loopBoundConst_float)
+				Instruction *k = BinaryOperator::CreateFMul(y, loopBoundConst_float, "");
+				k->insertAfter(loopBoundConst_float);
+
+				errs() << "k: " << *k << "\n";
+
+				//(int)(k)
+				Instruction *temp = FPToUIInst::Create(Instruction::CastOps::FPToUI, k,
+				Type::getInt32Ty(bb->getContext()), "");
+				temp->insertAfter(k);
+
+				errs() << "temp: " << *temp << "\n";
+
+				// // auto *zero = ConstantInt::get(Type::getInt32Ty(bb->getContext()), 0);					
+				// // errs() << "zero: " << *zero << "\n";
+
+				// // //int h = (int)(k)
+				// // Instruction *h = BinaryOperator::CreateAdd(temp, zero, "");
+				// // bb->getInstList().insert(bb->getFirstInsertionPt()->getIterator(), h);
+				
+				// //loopBoundConst->replaceAllUsesWith(temp);
+
+				for(auto &op : cmp->operands()){
+					if(op == indVar) continue;
+					errs() << "Truncation -- Changing [" << *op << "] to [" << *temp << "]!\n";
+					op = temp;
+				}
+				// //loopBoundConst->replaceAllUsesWith(temp);
+
+				errs() << "new_cmp" << *cmp << "\n\n";
+					
+			}
+			else if(auto* loopBoundConst = dyn_cast<ConstantInt>(loopBound)){
+				float loop_rate = 0;
+				loop_rate = InputLoopRate.getValue();
+				int value = (1 - loop_rate) * loopBoundConst->getZExtValue();
+				if(value <= 0) value = 1;
+
+				Type *ConstType = loopBound->getType();
+				Constant *NewInc = ConstantInt::get(ConstType, value /*value*/, true /*issigned*/);
+				
+				for(auto &op : cmp->operands()){
+					if(op == indVar) continue;
+					errs() << "Truncation -- Changing [" << *op << "] to [" << *NewInc << "]!\n\n";
+					op = NewInc;
+				}
+			}
+			else if(auto* loopBoundConst = dyn_cast<Argument>(loopBound)){
+				//TODO
+				errs() << "loop bound type argument!" << "\n";
+			}
+			else{
+				errs() << "loop bound type not found!" << "\n";
+			}	
+		}
+
 		//truncation perforation
 		void perforateTruncation(){
-			vector<Instruction *> vector_lb;
 			for (it_f_loop_map = f_loop_map.begin(); it_f_loop_map != f_loop_map.end(); ++it_f_loop_map){
 				auto loop = it_f_loop_map->first;
 				auto llvm_function = it_f_loop_map->second;
 
 				PHINode *indVar = loop->getCanonicalInductionVariable();
-				BinaryOperator *indVarUpdate = NULL;
 				ICmpInst *cmp = NULL;
 
 				for(auto &bb : loop->getBlocksVector()){
-					for (auto &lbInst : *bb){
-						auto *exitingBranch = dyn_cast<BranchInst>(&lbInst);
-						if (exitingBranch){
-							//branch must have a condition (which sets the loop bound)
-							if (exitingBranch->isConditional()){		
-								cmp = dyn_cast<ICmpInst>(exitingBranch->getCondition());						
-								if (cmp){
-									Value* loopBound;
-									for(auto &op : cmp->operands()){
-										if(auto *iv = dyn_cast<PHINode>(&op)) indVar = iv;
-										else loopBound = op;
+					if (BasicBlock *latchBlock = loop->getExitingBlock()){
+						for (auto &lbInst : *latchBlock){
+							auto *exitingBranch = dyn_cast<BranchInst>(&lbInst);
+							if (exitingBranch){
+								//branch must have a condition (which sets the loop bound)
+								if (exitingBranch->isConditional()){		
+									cmp = dyn_cast<ICmpInst>(exitingBranch->getCondition());						
+									if (cmp){
+										Value* loopBound;
+										for(auto &op : cmp->operands()){
+											if(auto *iv = dyn_cast<PHINode>(&op)) indVar = iv;
+											else loopBound = op;
+										}
+										if (loopBound != NULL){
+											perforateLoopBound(cmp, loopBound, indVar, latchBlock);
+										}
+										else
+											errs() << "no loop bound found!\n";
 									}
-
-									if (loopBound != NULL){
-										errs() << "cmp: " << *cmp << "\n\n";
-										errs() << "loopBound: " << *loopBound << "\n\n";									
-										if (auto *loopBoundConst = dyn_cast<Instruction>(loopBound)){	
-
-											errs() << "loopBoundConst: " << *loopBoundConst << "\n";
-											errs() << "indVar: " << *indVar << "\n";									
-
-											//float y = (1 - perforation_rate)
-											float value = (1 - InputLoopRate.getValue());
-											//errs() << "value: " << value << "\n\n";
-											//if(v <= 0) v = 1;
-											auto *y = ConstantFP::get(Type::getFloatTy(bb->getContext()), value);					
-											errs() << "y: " << *y << "\n";
-
-											// (float)(loopBoundConst)
-											Instruction *loopBoundConst_float = SIToFPInst::Create(Instruction::CastOps::SIToFP, 
-											loopBoundConst,	Type::getFloatTy(bb->getContext()), "");
-											
-											loopBoundConst_float->insertAfter(loopBoundConst);
-
-											errs() << "loopBoundConst_float: " << *loopBoundConst_float << "\n";
-											
-											// float k = (y * loopBoundConst_float)
-											Instruction *k = BinaryOperator::CreateFMul(y, loopBoundConst_float, "");
-											k->insertAfter(loopBoundConst_float);
-
-											errs() << "k: " << *k << "\n";
-
-											//(int)(k)
-											Instruction *temp = FPToUIInst::Create(Instruction::CastOps::FPToUI, k,
-											Type::getInt32Ty(bb->getContext()), "");
-											temp->insertAfter(k);
-
-											errs() << "temp: " << *temp << "\n";
-
-											// // auto *zero = ConstantInt::get(Type::getInt32Ty(bb->getContext()), 0);					
-											// // errs() << "zero: " << *zero << "\n";
-
-											// // //int h = (int)(k)
-											// // Instruction *h = BinaryOperator::CreateAdd(temp, zero, "");
-											// // bb->getInstList().insert(bb->getFirstInsertionPt()->getIterator(), h);
-											
-											// //loopBoundConst->replaceAllUsesWith(temp);
-
-											for(auto &op : cmp->operands()){
-												if(op == indVar) continue;
-												errs() << "Truncation -- Changing [" << *op << "] to [" << *temp << "]!\n";
-												op = temp;
-											}
-											// //loopBoundConst->replaceAllUsesWith(temp);
-
-											errs() << "new_cmp" << *cmp << "\n\n";
-												
-										}
-										else if(auto* loopBoundConst = dyn_cast<ConstantInt>(loopBound)){
-											float loop_rate = 0;
-											loop_rate = InputLoopRate.getValue();
-											int value = (1 - loop_rate) * loopBoundConst->getZExtValue();
-											if(value <= 0) value = 1;
-
-											Type *ConstType = loopBound->getType();
-											Constant *NewInc = ConstantInt::get(ConstType, value /*value*/, true /*issigned*/);
-											
-											for(auto &op : cmp->operands()){
-												if(op == indVar) continue;
-												errs() << "Truncation -- Changing [" << *op << "] to [" << *NewInc << "]!\n\n";
-												op = NewInc;
-											}
-										}
-										else if(auto* loopBoundConst = dyn_cast<Argument>(loopBound)){
-											//TODO
-											errs() << "loop bound type argument!" << "\n";
-										}
-										else{
-											errs() << "loop bound type not found!" << "\n";
-										}	
-									}
-									else
-										errs() << "no loop bound found!\n";
 								}
 							}
-							// else
-							// 	errs() << "exiting branch has no conditional!";
 						}
-						// else
-						// 	errs() << "no exiting branch!\n";
-					}
-					// for(auto *loopBoundConst : vector_lb){
-	
-					// }
-				}		
+					}	
+				}	
 			}
 		}
 
