283917a14c3a4d5cfcef8b1e7edadf69fec667d2
Lavinia Miranda <laviniameds@icloud.com>
2021-10-25 11:19:30 -0300

    update run.sh

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ current_dir="$( cd "$( dirname "$0" )" && pwd )"
 #get perforation rates
 rates_path="${current_dir}/settings/perforation_rates.txt"
 rates=$(<${rates_path})
+FLAGS=$2
 
 #get all files in dir 
 directories=$(find $1 -type d -name "*_results" )
@@ -19,7 +20,7 @@ for d in $directories; do
         mkdir -p ${dir_files}/bin
         files=$(find $dir_files -type f -name "*.opt.ll" )  
         #echo $files
-        clang++-12 -fPIC ${dir_files}/*.opt.ll -o ${dir_files}/bin/${base_name}_${i}.out
+        clang++-12 ${FLAGS} ${dir_files}/*.opt.ll -o ${dir_files}/bin/${base_name}_${i}.out
     done
 done 
 } 2> run.err
