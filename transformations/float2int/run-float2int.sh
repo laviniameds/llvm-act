4dc315cdf7afd324589f1413df2706b552729a94
Lavinia Miranda <laviniameds@icloud.com>
2021-10-27 10:27:19 -0300

    testing float2int llvm pass

---
+++ transformations/float2int/run-float2int.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# check if directory 'build' exists, if true: proceeds, else: create directory
+if [ -d build ]; then :  
+else mkdir build 
+fi 
+
+{
+# build project
+cd build/ && cmake .. && make && cd .. && echo ""\
+
+#get directories
+dir=$1
+current_dir="$( cd "$( dirname "$0" )" && cd .. && cd .. && pwd )"
+
+#get all files in dir 
+files=$(find $dir -type f \( -iname \*.c -o -iname \*.cpp \))
+#run through files in dir
+for src in $files; do
+    src_dir=$(dirname $src)
+    src_base=$(basename $src)
+    dir_path="${src_dir}/float2int_results"
+    mkdir -p $dir_path
+    filename=${dir_path}/${src_base%.*}.ll
+    opt=${dir_path}/${src_base%.*}.opt.ll
+    clang-12 -x c++ -S -emit-llvm ${src} -g3 -O0 -Xclang -disable-O0-optnone -o ${filename}
+    opt-12 -S -mem2reg -loop-simplify ${filename} > ${opt}
+
+    # if [ "${base_src_dir}" == "${base_filename%.*}" ]
+    # then
+        #echo ${base_src_dir} ${base_filename%.*}
+        #for i in $rates; do
+            dir_path="${src_dir}/float2int_results/"
+            mkdir -p $dir_path
+            opt_perf=${dir_path}/${src_base%.*}_modified.opt.ll    
+            #echo $opt_perf       
+            opt-12 -S -load build/transformations/float2int/libFloat2IntLegacyPass.so -float2int < ${opt} > ${opt_perf}      
+            #opt-12 -analyze -dot-cfg ${opt}
+            echo "Float2Int done! You can find results in ${dir_path}"        
+        #done
+    # fi
+done 
+} 2> float2int.err
